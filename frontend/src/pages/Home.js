3030fb2ce067629a7d1dd74bd8cb0cebc70a3e4f
Vinh Nguyen <nguyenvinh.fit@gmail.com>
2023-03-26 11:23:23 +0700

    Add movie list , integrate API  movie

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,63 @@
-import PageContent from '../components/PageContent';
+import React, { useState, useEffect, useCallback } from "react";
+
+import PageContent from "../components/PageContent";
+import MoviesList from "../components/MoviesList";
+import "./Home.css";
 
 function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDataHandler = useCallback(async () => {
+    setIsloading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong !!!");
+      }
+      const data = await response.json();
+
+      const tranformData = data?.results?.map((d) => {
+        return {
+          id: d.episode_id,
+          title: d.title,
+          openingText: d.opening_crawl,
+          releaseDate: d.release_date,
+        };
+      });
+      console.log("tranformData", tranformData);
+      setMovies(tranformData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsloading(false);
+
+    // .catch((err) => console.log("err", err));
+  }, []);
+
+  useEffect(() => {
+    fetchDataHandler();
+  }, [fetchDataHandler]);
+
+  let content = <p> No found Movies ... </p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading ...</p>;
+  }
+
   return (
     <PageContent title="Welcome!">
-      <p>Browse all our amazing events!</p>
+      <section>
+        <button onClick={fetchDataHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
     </PageContent>
   );
 }
